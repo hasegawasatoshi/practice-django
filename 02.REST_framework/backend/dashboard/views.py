ba4c29f8d6bd105841a5d8db172ca17a08fd2b4b
hasegawasatoshi <hase919gawa@gmail.com>
2021-07-26 12:39:36 +0900

    Added sensor list page

--- 02.REST_framework/backend/dashboard/views.py
+++ 02.REST_framework/backend/dashboard/views.py
@@ -9,6 +9,5 @@ def sensor_list(request):
     if request.method == 'GET':
         sensor = Sensor.objects.all()
         serializer = SensorSerializer(sensor, many=True)
-        print(serializer.data)
         content = {'sensor_list': json.loads(json.dumps(serializer.data))}
         return render(request, 'dashboard/sensor_list.html', content)
